6a885abaede4d2e2b21cf319ae695f615215a2df
Kolla Rajita <rajitakolla@gmail.com>
2024-08-10 14:04:49 -0400

    Code changes for Fruits into basket problem

---
+++ algos/Fruits into Basket.py
@@ -0,0 +1,18 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+
+        max_picked = 0
+        n = len(fruits)
+        for i in range(0,n):
+            basket = set()
+            for j in range(i, n):
+                basket.add(fruits[j])
+                if len(basket) <= 2:
+                    max_picked = max(max_picked, j-i+1)
+                elif len(basket) == 2:
+                    break
+
+        return max_picked
+
+
+
