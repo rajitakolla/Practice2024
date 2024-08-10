d31b75fcd387d035d3feaca7a77a8a94e6c974c0
Kolla Rajita <rajitakolla@gmail.com>
2024-08-10 11:08:51 -0400

    setup

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a repository for LeetCode solutions
+This is a repository for LeetCode solutions. These are ran on Leetcode platform
--- algos/Unique Email Addresses.py
+++ algos/Unique Email Addresses.py
@@ -1,1 +1,7 @@
-print("abc")
+#https://leetcode.com/explore/interview/card/google/67/sql-2/3044/
+from typing import List
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        split_domain_name = lambda x : x.split("@")
+        res = [split_domain_name(email)[0].replace(".",'').split("+")[0]+"@"+split_domain_name(email)[1] for email in emails]
+        return len(set(res))
